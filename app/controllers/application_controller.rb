7e9cebb1e2336f691a501538967315bee6f636b2
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-25 13:56:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,11 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
+  patch '/recipes/:id' do
+      @recipe = Recipe.find(params[:id])
+      @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+  end
+
   delete '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
     @recipe.delete
