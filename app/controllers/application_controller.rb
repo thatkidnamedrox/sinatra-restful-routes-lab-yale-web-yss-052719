086c453610d8ab28b291f3406df75ad8ca77c7cc
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-25 13:59:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,39 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to("/recipes/#{@recipe.id}")
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+      @recipe = Recipe.find(params[:id])
+      @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+      redirect to("/recipes/#{@recipe.id}")
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+  end
 
 end
